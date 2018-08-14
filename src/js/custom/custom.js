ea5716e1edc9d358d47955f734ae3051bc85895e
Khiyirbeg <akhiyirbeg@mail.ru>
2018-08-14 17:36:38 +0300

    Adding the lower menu and its styles

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -1,11 +1,24 @@
 $(function() {
 
+/*menu__top-main*/
 
 $('nav').coreNavigation({
     menuPosition: "center",
     container: true,
     mode: 'sticky'
 });
+
+/*menu__bottom*/
+var home = document.querySelector('.menu__bottom-icon');
+home.addEventListener('click', collapsible);
+
+function collapsible() {
+    document.querySelector('#menu__bottom')
+            .classList
+            .toggle("show");
+}
+
+
 
 
 });
