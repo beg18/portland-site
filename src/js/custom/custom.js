d342c92d5989dd14e3d53cb26769fc30bf6e7317
Khiyirbeg <akhiyirbeg@mail.ru>
2018-08-16 15:43:31 +0300

    Added modal window and all of its functional

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -66,6 +66,28 @@ function collapsible() {
         radioClass: 'iradio_flat-green',
     });
 
+/* Modals
+    ====================================*/
+
+    $(".js-show-modal").on("click", function(e) {
+
+        e.preventDefault();
+
+        var currentModal = $(this).attr("href");
+
+        $(currentModal + ", #js-overlay").fadeIn(500);
+        $("body").addClass("open-modal");
+
+    });
+
+
+    $("#js-overlay, .js-modal-close").on("click", function(e) {
+
+        e.preventDefault();
+        $(".js-modal, #js-overlay").fadeOut(100);
+        $("body").removeClass("open-modal");
+
+    });
 
 
 });
